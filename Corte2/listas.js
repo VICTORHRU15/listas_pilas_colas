610fe7ed22d0a3fbb8d6c941343e1527cc88a0c0
VICTORHRU15 <victorhru15@gmail.com>
2026-03-16 20:35:21 -0500

    Subir archivos

--- Corte2/listas.js
+++ Corte2/listas.js
@@ -1,1 +1,28 @@
-console.log("Listas,pilas y colas")
+/* //Recorres una lista
+for (let i = 0; i < lista.length; i ++){
+console.log(lista[i]);
+
+}
+
+//recorres una lista con for of
+
+for (let elemento of lista){
+    console.log(elemento);
+
+} */
+
+
+let mercado = ["Tomate","Arroz","Sal"];
+
+mercado.unshift("Azucar");
+
+for (let i = 0; i < mercado.length; i ++){
+console.log(mercado[i]);
+
+}
+
+mercado.pop();
+
+
+console.log(mercado);
+
---
+++ Corte2/pilas.js
@@ -0,0 +1,15 @@
+// Principio LIFO (Last in, First Out)
+
+let pila = []
+
+//agregar elementos a la pila
+
+pila.push(1);
+pila.push(2);
+pila.push(3);
+console.log(pila);
+
+//Eliminar elementos de la pila
+
+let elementoseliminado = pila.pop();
+console.log (elementoseliminado);
